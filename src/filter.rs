d7d6d6b275ccfa0d16fb638a7e6bb6754a586d77
Liam <liam@scalzulli.com>
2021-12-15 01:00:27 -0500

    format

--- src/filter.rs
+++ src/filter.rs
@@ -37,54 +37,53 @@ impl Filter {
       // }
       Self::Generate { width, height } => {
         state.generate(*width, *height);
-      }
-      // Self::Invert => {
-      //   state
-      //     .scalars_mut()
-      //     .iter_mut()
-      //     .for_each(|scalar| *scalar = !*scalar);
-      // }
-      // Self::Modulus { divisor, remainder } => {
-      //   state
-      //     .scalars_mut()
-      //     .iter_mut()
-      //     .enumerate()
-      //     .for_each(|(i, scalar)| {
-      //       if i % divisor == *remainder {
-      //         *scalar = !*scalar;
-      //       }
-      //     })
-      // }
-      // Self::Square => {
-      //   let (width, height) = state.dimensions();
-      //   let (x1, y1) = (width / 4, height / 4);
-      //   let (x2, y2) = (x1 + width / 2, y1 + height / 2);
-      //   state.rows_mut().enumerate().for_each(|(row, line)| {
-      //     line.iter_mut().enumerate().for_each(|(col, scalar)| {
-      //       if col / 3 > x1 && col / 3 < x2 && row > y1 && row < y2 {
-      //         *scalar = !*scalar;
-      //       }
-      //     })
-      //   });
-      // }
-      // Self::Pixelate { size } => {
-      //   for row in 0..state.height() {
-      //     for col in 0..state.width() {
-      //       let source_row = row / size * size + size / 2;
-      //       let source_col = col / size * size + size / 2;
-      //       let source_pixel = state.get_pixel(source_row, source_col);
-      //       state.set_pixel(row, col, source_pixel);
-      //     }
-      //   }
-      // }
-      // Self::Top => {
-      //   let height = state.height();
-      //   state.rows_mut().enumerate().for_each(|(i, row)| {
-      //     if i < height / 2 {
-      //       row.iter_mut().for_each(|scalar| *scalar = !*scalar);
-      //     }
-      //   });
-      // }
+      } // Self::Invert => {
+        //   state
+        //     .scalars_mut()
+        //     .iter_mut()
+        //     .for_each(|scalar| *scalar = !*scalar);
+        // }
+        // Self::Modulus { divisor, remainder } => {
+        //   state
+        //     .scalars_mut()
+        //     .iter_mut()
+        //     .enumerate()
+        //     .for_each(|(i, scalar)| {
+        //       if i % divisor == *remainder {
+        //         *scalar = !*scalar;
+        //       }
+        //     })
+        // }
+        // Self::Square => {
+        //   let (width, height) = state.dimensions();
+        //   let (x1, y1) = (width / 4, height / 4);
+        //   let (x2, y2) = (x1 + width / 2, y1 + height / 2);
+        //   state.rows_mut().enumerate().for_each(|(row, line)| {
+        //     line.iter_mut().enumerate().for_each(|(col, scalar)| {
+        //       if col / 3 > x1 && col / 3 < x2 && row > y1 && row < y2 {
+        //         *scalar = !*scalar;
+        //       }
+        //     })
+        //   });
+        // }
+        // Self::Pixelate { size } => {
+        //   for row in 0..state.height() {
+        //     for col in 0..state.width() {
+        //       let source_row = row / size * size + size / 2;
+        //       let source_col = col / size * size + size / 2;
+        //       let source_pixel = state.get_pixel(source_row, source_col);
+        //       state.set_pixel(row, col, source_pixel);
+        //     }
+        //   }
+        // }
+        // Self::Top => {
+        //   let height = state.height();
+        //   state.rows_mut().enumerate().for_each(|(i, row)| {
+        //     if i < height / 2 {
+        //       row.iter_mut().for_each(|scalar| *scalar = !*scalar);
+        //     }
+        //   });
+        // }
     }
   }
 }
