d710daab7ee3da11f6c3c93cdd4657366eafed72
Casey Rodarmor <casey@rodarmor.com>
2021-12-22 18:02:13 -0600

    Add loop command

--- src/command.rs
+++ src/command.rs
@@ -1,12 +1,16 @@
 use super::*;
 
+#[derive(Clone, Debug)]
 pub(crate) enum Command {
   Filter(Filter),
+  For(usize),
+  Loop,
   Operation(Operation),
   Print,
   Repl,
-  Resize { rows: usize, cols: usize },
-  Save { path: PathBuf },
+  Resize((usize, usize)),
+  Save(PathBuf),
+  Verbose,
 }
 
 impl Command {
@@ -15,18 +19,42 @@ impl Command {
       Self::Filter(filter) => {
         for col in 0..state.matrix.ncols() {
           for row in 0..state.matrix.nrows() {
-            if filter.filter(state, col, row) {
+            if filter.filter(state, row, col) {
               state.matrix[(row, col)] = state.operation.apply(state, state.matrix[(row, col)]);
             }
           }
         }
+      }
+      Self::For(until) => {
+        if state.loop_counter >= *until {
+          loop {
+            state.program_counter = state.program_counter.wrapping_add(1);
+            if let Some(Self::Loop) | None = state.program.get(state.program_counter) {
+              break;
+            }
+          }
+          state.loop_counter = 0;
+        }
+      }
+      Self::Loop => {
+        loop {
+          state.program_counter = state.program_counter.wrapping_sub(1);
+          let next = state.program_counter.wrapping_add(1);
+          if next == 0 {
+            break;
+          }
+          if let Some(Self::For(_)) | None = state.program.get(next) {
+            break;
+          }
+        }
+        state.loop_counter += 1;
       }
       Self::Operation(operation) => state.operation = *operation,
       Self::Print => state.print()?,
       Self::Repl => {
         for result in BufReader::new(io::stdin()).lines() {
           let line = result?;
-          match line.trim().parse::<Command>() {
+          match line.trim().parse::<Self>() {
             Ok(command) => {
               command.apply(state)?;
               state.print()?;
@@ -37,10 +65,11 @@ impl Command {
           }
         }
       }
-      Self::Resize { rows, cols } => {
-        state.resize(Vector2::new(*rows, *cols));
+      Self::Resize(dimensions) => {
+        state.resize(*dimensions);
       }
-      Self::Save { path } => state.image()?.save(path)?,
+      Self::Save(path) => state.image()?.save(path)?,
+      Self::Verbose => state.verbose = !state.verbose,
     }
 
     Ok(())
@@ -55,7 +84,9 @@ impl FromStr for Command {
       ["all"] => Ok(Self::Filter(Filter::All)),
       ["circle"] => Ok(Self::Filter(Filter::Circle)),
       ["even"] => Ok(Self::Filter(Filter::Even)),
+      ["for", count] => Ok(Self::For(count.parse()?)),
       ["invert"] => Ok(Self::Operation(Operation::Invert)),
+      ["loop"] => Ok(Self::Loop),
       ["mod", divisor, remainder] => Ok(Self::Filter(Filter::Mod {
         divisor: divisor.parse()?,
         remainder: remainder.parse()?,
@@ -63,15 +94,11 @@ impl FromStr for Command {
       ["print"] => Ok(Self::Print),
       ["random"] => Ok(Self::Operation(Operation::Random)),
       ["repl"] => Ok(Self::Repl),
-      ["resize", cols, rows] => Ok(Self::Resize {
-        cols: cols.parse()?,
-        rows: rows.parse()?,
-      }),
-      ["save", path] => Ok(Self::Save {
-        path: path.parse()?,
-      }),
+      ["resize", cols, rows] => Ok(Self::Resize((rows.parse()?, cols.parse()?))),
+      ["save", path] => Ok(Self::Save(path.parse()?)),
       ["square"] => Ok(Self::Filter(Filter::Square)),
       ["top"] => Ok(Self::Filter(Filter::Top)),
+      ["verbose"] => Ok(Self::Verbose),
       _ => Err(format!("Invalid command: {}", s).into()),
     }
   }
--- src/state.rs
+++ src/state.rs
@@ -6,22 +6,32 @@ use {
 };
 
 pub(crate) struct State {
+  pub(crate) loop_counter: usize,
   pub(crate) matrix: DMatrix<Vector3<u8>>,
   pub(crate) operation: Operation,
+  pub(crate) program: Vec<Command>,
+  pub(crate) program_counter: usize,
   pub(crate) rng: StdRng,
+  pub(crate) verbose: bool,
 }
 
 impl State {
   pub(crate) fn run() -> Result<()> {
-    let commands = env::args()
-      .skip(1)
-      .map(|arg| arg.parse())
-      .collect::<Result<Vec<Command>>>()?;
-
     let mut state = Self::new();
 
-    for command in commands {
+    for arg in env::args().skip(1) {
+      state.program.push(arg.parse()?);
+    }
+
+    while let Some(command) = state.program.get(state.program_counter).cloned() {
+      if state.verbose {
+        eprintln!(
+          "PC {} LC {} {:?}",
+          state.program_counter, state.loop_counter, command
+        );
+      }
       command.apply(&mut state)?;
+      state.program_counter = state.program_counter.wrapping_add(1);
     }
 
     Ok(())
@@ -29,18 +39,24 @@ impl State {
 
   pub(crate) fn new() -> Self {
     Self {
+      loop_counter: 0,
       matrix: DMatrix::zeros(0, 0),
       operation: Operation::Invert,
+      program: Vec::new(),
+      program_counter: 0,
       rng: StdRng::seed_from_u64(0),
+      verbose: false,
     }
   }
 
-  pub(crate) fn resize(&mut self, dim: Vector2<usize>) {
-    self.matrix.resize_mut(dim.x, dim.y, Zero::zero())
+  pub(crate) fn resize(&mut self, dimensions: (usize, usize)) {
+    self
+      .matrix
+      .resize_mut(dimensions.0, dimensions.1, Zero::zero())
   }
 
-  pub(crate) fn dimensions(&self) -> Vector2<usize> {
-    Vector2::new(self.matrix.ncols(), self.matrix.nrows())
+  pub(crate) fn dimensions(&self) -> (usize, usize) {
+    (self.matrix.nrows(), self.matrix.ncols())
   }
 
   pub(crate) fn image(&self) -> Result<RgbImage> {
