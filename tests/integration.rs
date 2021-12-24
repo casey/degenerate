7162ff3a757cb9fe532a82046afe6800574c6b0d
Liam <liam@scalzulli.com>
2021-12-23 21:47:08 -0500

    fix test

--- tests/integration.rs
+++ tests/integration.rs
@@ -175,18 +175,20 @@ fn repl_valid_filter() -> Result<()> {
 }
 
 #[test]
-fn repl_exit_after_error() -> Result<()> {
+fn repl_invalid_filter() -> Result<()> {
   let mut command = Command::new(executable_path("degenerate"))
-    .args(["resize:4:4", "repl", "print"])
+    .args(["resize:4:4", "repl"])
     .stdin(Stdio::piped())
     .stdout(Stdio::piped())
     .stderr(Stdio::piped())
     .spawn()?;
 
-  let stdin = command.stdin.as_mut().unwrap();
-  write!(stdin, "invalid")?;
+  write!(command.stdin.as_mut().unwrap(), "invalid")?;
 
-  assert_eq!(str::from_utf8(&command.wait_with_output()?.stdout)?, "");
+  assert_eq!(
+    str::from_utf8(&command.wait_with_output()?.stderr)?,
+    "Could not parse command from `invalid`: Invalid command: invalid\n"
+  );
 
   Ok(())
 }
