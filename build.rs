d6cb35aa27b9c72aef8d6469c830e95f3b1e6a43
Casey Rodarmor <casey@rodarmor.com>
2021-12-29 12:15:41 -0800

    use char::is_alphanumeric

--- build.rs
+++ build.rs
@@ -24,7 +24,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
       .file_stem()
       .ok_or_else(|| format!("Could not extract file stem: {}", expected_path))?;
 
-    let identifier = program.replace(|c| !matches!(c, 'a'..='z' | '0'..='9'), "_");
+    let identifier = program.replace(|c| !c.is_alphanumeric(), "_");
 
     write!(
       file,
