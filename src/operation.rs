a5a4032ee7299416afe4d73621d29c4083c3a1f1
Casey Rodarmor <casey@rodarmor.com>
2021-12-29 13:02:30 -0600

    Remove random operation (#83)

--- src/operation.rs
+++ src/operation.rs
@@ -3,15 +3,13 @@ use super::*;
 #[derive(Copy, Clone, Debug)]
 pub(crate) enum Operation {
   Invert,
-  Random,
   RotateColor(ColorAxis, f64),
 }
 
 impl Operation {
-  pub(crate) fn apply(self, rng: &mut impl Rng, element: Vector3<u8>) -> Vector3<u8> {
+  pub(crate) fn apply(self, element: Vector3<u8>) -> Vector3<u8> {
     match self {
       Self::Invert => element.map(|scalar| !scalar),
-      Self::Random => Vector3::new(rng.gen(), rng.gen(), rng.gen()),
       Self::RotateColor(axis, turns) => {
         let v = Rotation3::new(axis.vector() * turns * f64::consts::TAU)
           * element.map(|scalar| scalar as f64 / 255.0 * 2.0 - 1.0);
