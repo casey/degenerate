6c956cd42e17db9618e6f5f591e8464105ae7b4c
Liam <liam@scalzulli.com>
2021-12-31 15:00:50 -0500

    ci

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use {
   },
   dirs::home_dir,
   image::{ImageBuffer, RgbImage},
-  nalgebra::{DMatrix, Rotation2, Rotation3, Similarity2, UnitComplex, Vector2, Vector3},
+  nalgebra::{DMatrix, Rotation3, Similarity2, UnitComplex, Vector2, Vector3},
   rand::Rng,
   rand::{rngs::StdRng, SeedableRng},
   rustyline::{error::ReadlineError, Editor},
