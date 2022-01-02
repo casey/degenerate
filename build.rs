c736530578c5e2cb01a7622026521fa85f7f5789
Casey Rodarmor <casey@rodarmor.com>
2022-01-02 10:38:04 -0800

    Remove type alias

--- build.rs
+++ build.rs
@@ -7,9 +7,7 @@ use {
   },
 };
 
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-fn main() -> Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
   let mut programs = Vec::new();
 
   {
