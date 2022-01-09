00728dd6bb8e8043a089b98a501fe49d1af3f0fd
Liam <liam@scalzulli.com>
2022-01-08 21:39:59 -0500

    Get this to compile

--- src/mask.rs
+++ src/mask.rs
@@ -19,9 +19,9 @@ impl Mask {
       Self::Circle => v.norm() < 1.0,
       Self::Cross => v.x.abs() < 0.25 || v.y.abs() < 0.25,
       Self::Mod { divisor, remainder } => {
-        (pixel.x * state.matrix.nrows() + pixel.y) % divisor == *remainder
+        (pixel.x * state.matrix.nrows() as isize + pixel.y) % *divisor as isize == *remainder as isize
       }
-      Self::Rows { nrows, step } => pixel.y % (nrows.saturating_add(*step)) < *nrows,
+      Self::Rows { nrows, step } => pixel.y % (nrows.saturating_add(*step) as isize) < *nrows as isize,
       Self::Square => v.abs() < Vector2::new(0.5, 0.5),
       Self::Top => v.y < 0.0,
       Self::X => (v.x - v.y).abs() < 0.25 || (v.x + v.y).abs() < 0.25,
