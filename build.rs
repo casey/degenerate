88f21aceb521a29073d9cbf8392d44c8d155a544
liam <31192478+terror@users.noreply.github.com>
2022-05-03 18:31:53 -0400

    Remove image gallery from readme (#167)

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use {
   camino::Utf8PathBuf,
   indoc::indoc,
   std::{
-    fs::{self, DirEntry, File},
+    fs::{self, DirEntry},
     io::{self, Write},
   },
 };
@@ -10,45 +10,44 @@ use {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   let mut programs = Vec::new();
 
-  {
-    println!("cargo:rerun-if-changed=images");
+  println!("cargo:rerun-if-changed=images");
 
-    let mut file = fs::File::create("tests/image_tests.rs")?;
+  let mut file = fs::File::create("tests/image_tests.rs")?;
 
-    write!(file, "use super::*;")?;
+  write!(file, "use super::*;")?;
 
-    let mut entries = fs::read_dir("images")?.collect::<io::Result<Vec<DirEntry>>>()?;
-    entries.sort_by_key(|entry| entry.file_name());
+  let mut entries = fs::read_dir("images")?.collect::<io::Result<Vec<DirEntry>>>()?;
+  entries.sort_by_key(|entry| entry.file_name());
 
-    for entry in entries {
-      let expected_path = Utf8PathBuf::try_from(entry.path())?;
-      println!("cargo:rerun-if-changed={}", expected_path);
+  for entry in entries {
+    let expected_path = Utf8PathBuf::try_from(entry.path())?;
+    println!("cargo:rerun-if-changed={}", expected_path);
 
-      let filename = expected_path
-        .file_name()
-        .ok_or_else(|| format!("Could not extract file name: {}", expected_path))?;
+    let filename = expected_path
+      .file_name()
+      .ok_or_else(|| format!("Could not extract file name: {}", expected_path))?;
 
-      if !filename.ends_with(".png") || filename.ends_with(".actual-memory.png") {
-        continue;
-      }
+    if !filename.ends_with(".png") || filename.ends_with(".actual-memory.png") {
+      continue;
+    }
 
-      let name = expected_path
-        .file_stem()
-        .ok_or_else(|| format!("Could not extract file stem: {}", expected_path))?
-        .to_owned();
+    let name = expected_path
+      .file_stem()
+      .ok_or_else(|| format!("Could not extract file stem: {}", expected_path))?
+      .to_owned();
 
-      let program = fs::read_to_string(format!("images/{}.degen", name))?
-        .trim()
-        .to_owned();
+    let program = fs::read_to_string(format!("images/{}.degen", name))?
+      .trim()
+      .to_owned();
 
-      programs.push((name.clone(), program.clone()));
+    programs.push((name.clone(), program.clone()));
 
-      let identifier = format!("image{}", name);
+    let identifier = format!("image{}", name);
 
-      write!(
-        file,
-        indoc!(
-          "
+    write!(
+      file,
+      indoc!(
+        "
 
 
         #[test]{}
@@ -56,40 +55,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         fn {}() -> Result<()> {{
           image_test(\"{}\")
         }}",
-        ),
-        if program.contains("comment:slow") {
-          "\n#[ignore]"
-        } else {
-          ""
-        },
-        identifier,
-        name
-      )?;
-    }
-
-    writeln!(file)?;
+      ),
+      if program.contains("comment:slow") {
+        "\n#[ignore]"
+      } else {
+        ""
+      },
+      identifier,
+      name
+    )?;
   }
 
-  {
-    println!("cargo:rerun-if-changed=README.md");
-    let text = fs::read_to_string("README.md")?;
+  writeln!(file)?;
 
-    let mut file = File::create("README.md")?;
-
-    for line in text.lines() {
-      writeln!(file, "{}", line)?;
-
-      if line == "## Gallery" {
-        break;
-      }
-    }
-
-    for (name, program) in programs {
-      writeln!(file)?;
-      writeln!(file, "```\n$ degenerate {}\n```", program)?;
-      writeln!(file, "![{}](images/{}.png)", program, name)?;
-    }
-
-    Ok(())
-  }
+  Ok(())
 }
