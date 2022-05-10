2516232c149d79a9515343fbcf1f1a0068428d2d
Casey Rodarmor <casey@rodarmor.com>
2022-05-10 02:23:50 -0500

    tweak

--- src/native/display.rs
+++ src/native/display.rs
@@ -3,9 +3,11 @@ use super::*;
 pub(crate) struct Display;
 
 impl Display {
-  pub(crate) fn render(&self, _memory: &DMatrix<Vector3<u8>>) {}
+  pub(crate) fn render(&self, _memory: &DMatrix<Vector3<u8>>) -> Result {
+    Ok(())
+  }
 
-  pub(crate) fn dimensions(&self) -> (usize, usize) {
-    (256, 256)
+  pub(crate) fn dimensions(&self) -> Result<(usize, usize)> {
+    Ok((256, 256))
   }
 }
