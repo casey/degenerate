b7ae097cdc83afe32826524b130f4d2c93192eb3
Casey Rodarmor <casey@rodarmor.com>
2022-05-09 17:34:16 -0700

    Write some slightly nicer helpers

--- src/browser.rs
+++ src/browser.rs
@@ -1,43 +1,58 @@
 use {
   super::*,
   wasm_bindgen::{closure::Closure, JsCast},
-  web_sys::{CanvasRenderingContext2d, HtmlCanvasElement, HtmlTextAreaElement},
+  web_sys::{CanvasRenderingContext2d, Document, Element, HtmlCanvasElement, HtmlTextAreaElement},
 };
-
-pub(crate) mod display;
 
 // todo:
 // - remove unwraps
 // - surface errors to user
 // - decide canvas layout
 
+pub(crate) mod display;
+
 pub(crate) fn run() {
+  run_inner().unwrap();
+}
+
+trait Select {
+  fn select(&self, selector: &str) -> Result<Element>;
+}
+
+impl Select for Document {
+  fn select(&self, selector: &str) -> Result<Element> {
+    Ok(self.query_selector(selector).unwrap().unwrap())
+  }
+}
+
+trait Cast {
+  fn cast<T: JsCast>(self) -> Result<T>;
+}
+
+impl<V: JsCast + std::fmt::Debug> Cast for V {
+  fn cast<T: JsCast>(self) -> Result<T> {
+    Ok(self.dyn_into::<T>().unwrap())
+  }
+}
+
+fn run_inner() -> Result<(), Error> {
   console_error_panic_hook::set_once();
 
-  let window = web_sys::window().unwrap();
+  let window = web_sys::window().ok_or_else(|| "window missing".to_string())?;
 
-  let document = window.document().unwrap();
+  let document = window
+    .document()
+    .ok_or_else(|| "window.document missing".to_string())?;
 
-  let textarea = document
-    .query_selector("textarea")
-    .unwrap()
-    .unwrap()
-    .dyn_into::<HtmlTextAreaElement>()
-    .unwrap();
+  let textarea = document.select("textarea")?.cast::<HtmlTextAreaElement>()?;
 
-  let canvas = document
-    .query_selector("canvas")
-    .unwrap()
-    .unwrap()
-    .dyn_into::<HtmlCanvasElement>()
-    .unwrap();
+  let canvas = document.select("canvas")?.cast::<HtmlCanvasElement>()?;
 
   let context = canvas
     .get_context("2d")
     .unwrap()
     .unwrap()
-    .dyn_into::<CanvasRenderingContext2d>()
-    .unwrap();
+    .cast::<CanvasRenderingContext2d>()?;
 
   let display = Display { context };
 
@@ -53,4 +68,6 @@ pub(crate) fn run() {
   cb.forget();
 
   web_sys::console::log_1(&"hello".into());
+
+  Ok(())
 }
