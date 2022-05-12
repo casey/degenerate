91b1f13de34c8a0a0c8b76fff6cda5bbe0468972
Casey Rodarmor <casey@rodarmor.com>
2022-05-12 12:32:49 -0700

    Update on animation frames

--- src/browser/app.rs
+++ src/browser/app.rs
@@ -7,6 +7,9 @@ pub(crate) struct App {
   window: Window,
   textarea: HtmlTextAreaElement,
   display: Display,
+  input: bool,
+  animation_frame_callback: Option<Closure<dyn FnMut()>>,
+  stderr: Stderr,
 }
 
 impl App {
@@ -34,27 +37,68 @@ impl App {
       resize: true,
       display: Display { context },
       textarea: textarea.clone(),
+      input: false,
+      animation_frame_callback: None,
+      stderr: stderr.clone(),
       nav,
       canvas,
     }));
 
     let local = app.clone();
-    window.add_event_listener("resize", move || local.lock().unwrap().on_resize())?;
+    app.lock().unwrap().animation_frame_callback = Some(Closure::wrap(Box::new(move || {
+      let mut app = local.lock().unwrap();
+      let result = app.on_animation_frame();
+      app.stderr.update(result);
+    }) as Box<dyn FnMut()>));
 
-    textarea.add_event_listener("input", move || match app.lock().unwrap().on_input() {
-      Err(err) => stderr.set(err.as_ref()),
-      Ok(()) => stderr.clear(),
+    let local = app.clone();
+    window.add_event_listener("resize", move || {
+      let mut app = local.lock().unwrap();
+      let result = app.on_resize();
+      app.stderr.update(result);
+    })?;
+
+    textarea.add_event_listener("input", move || {
+      let mut app = app.lock().unwrap();
+      let result = app.on_input();
+      stderr.update(result);
     })?;
 
     Ok(())
   }
 
-  pub(super) fn on_resize(&mut self) {
+  pub(super) fn on_resize(&mut self) -> Result {
     log!("resize");
     self.resize = true;
+    self.request_animation_frame()?;
+    Ok(())
   }
 
   pub(super) fn on_input(&mut self) -> Result {
+    log!("input");
+    self.input = true;
+    self.request_animation_frame()?;
+    Ok(())
+  }
+
+  fn request_animation_frame(&self) -> Result {
+    self
+      .window
+      .request_animation_frame(
+        &self
+          .animation_frame_callback
+          .as_ref()
+          .unwrap()
+          .as_ref()
+          .dyn_ref()
+          .unwrap(),
+      )
+      .map_err(|err| format!("`window.requestAnimationFrame` failed: {:?}", err))?;
+    Ok(())
+  }
+
+  fn on_animation_frame(&mut self) -> Result {
+    log!("animation frame");
     if self.resize {
       let css_pixel_height: f64 = self.canvas.client_height().try_into()?;
       let css_pixel_width: f64 = self.canvas.client_width().try_into()?;
@@ -75,7 +119,12 @@ impl App {
       self.canvas.set_width(width.ceil() as u32);
       self.resize = false;
     }
-    self.nav.set_class_name("fade-out");
-    Computer::run(&self.display, self.textarea.value().split_whitespace())
+
+    if self.input {
+      self.nav.set_class_name("fade-out");
+      Computer::run(&self.display, self.textarea.value().split_whitespace())?;
+    }
+
+    Ok(())
   }
 }
--- src/browser/stderr.rs
+++ src/browser/stderr.rs
@@ -13,6 +13,13 @@ impl Stderr {
     ))
   }
 
+  pub(crate) fn update(&self, result: Result) {
+    match result {
+      Err(err) => self.set(err.as_ref()),
+      Ok(()) => self.clear(),
+    }
+  }
+
   pub(crate) fn set(&self, err: &dyn std::error::Error) {
     self.0.set_text_content(Some(&err.to_string()));
   }
