b5b3d9060cca5b8a4529c3b19bfb810cbf1a3807
casey <casey@users.noreply.github.com>
2022-05-20 19:01:23 +0000

    deploy: 1e0af7c2c442e6a0363509ed6495f30a9b055dc8

--- degenerate.js
+++ degenerate.js
@@ -491,11 +491,11 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper58 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper59 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 3, __wbg_adapter_18);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper60 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper61 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 3, __wbg_adapter_21);
         return addHeapObject(ret);
     };
