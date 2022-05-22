882148674264718069b739af8e89e1f113d66fdf
Casey Rodarmor <casey@rodarmor.com>
2022-05-21 21:10:39 -0700

    Make position attribute vec2

--- src/vertex.glsl
+++ src/vertex.glsl
@@ -1,9 +1,9 @@
 #version 300 es
 
-in vec4 position;
+in vec2 position;
 out vec2 uv;
 
 void main() {
   uv = position.xy * 0.5 + 0.5;
-  gl_Position = position;
+  gl_Position = vec4(position, 0.0, 1.0);
 }
