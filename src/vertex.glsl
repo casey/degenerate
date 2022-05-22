98fac74ec6798b09d7f1ec51f536aebcad89ed2b
Casey Rodarmor <casey@rodarmor.com>
2022-05-21 21:45:52 -0700

    Refactor vertex shader a little bit

--- src/vertex.glsl
+++ src/vertex.glsl
@@ -1,9 +1,14 @@
 #version 300 es
 
+// Generate a single screen-filling triangle.
+// See this post for details:
+// https://stackoverflow.com/a/59739538/66450
+
 out vec2 uv;
 
+const vec2 vertices[3]= vec2[3](vec2(-1.0, -1.0), vec2(3.0, -1.0), vec2(-1.0, 3.0));
+
 void main() {
-  vec2 vertices[3]= vec2[3](vec2(-1, -1), vec2(3, -1), vec2(-1, 3));
-  gl_Position = vec4(vertices[gl_VertexID], 0, 1);
-  uv = 0.5 * gl_Position.xy + vec2(0.5);
+  gl_Position = vec4(vertices[gl_VertexID], 0.0, 1.0);
+  uv = (gl_Position.xy + 1.0) / 2.0;
 }
