01e3e1dbbe4d4a241f8ae2bea725dd36235944ea
liam <31192478+terror@users.noreply.github.com>
2022-05-23 16:48:58 -0400

    Implement cross mask for GPU  (#297)

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -28,6 +28,8 @@ bool is_masked(vec2 position) {
       return true;
     case CIRCLE:
       return length(position) < 1.0;
+    case CROSS:
+      return abs(position.x) < 0.25 || abs(position.y) < 0.25;
     case SQUARE:
       return abs(position.x) < 0.5 && abs(position.y) < 0.5;
     case X:
