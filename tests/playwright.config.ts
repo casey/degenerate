d45bb4ed5d25b4158aa6897cc8f347c9784e4b11
Liam <liam@scalzulli.com>
2022-06-02 18:07:21 -0400

    Cargo cult

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -4,7 +4,7 @@ import { devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   expect: { timeout: 5000 },
   forbidOnly: !!process.env.CI,
-  fullyParallel: false,
+  fullyParallel: true,
   globalSetup: require.resolve('./global-setup'),
   projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }],
   reporter: [['html', { open: 'never' }]],
