9b5d2101fd4ad11ce980f7621fead09e69d0853d
Liam <liam@scalzulli.com>
2022-06-02 17:48:36 -0400

    One worker, no retries on CI

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -4,14 +4,15 @@ import { devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   expect: { timeout: 5000 },
   forbidOnly: !!process.env.CI,
-  fullyParallel: true,
+  fullyParallel: false,
   globalSetup: require.resolve('./global-setup'),
   projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }],
   reporter: [['html', { open: 'never' }]],
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   testDir: '.',
   timeout: 30 * 1000,
   use: { actionTimeout: 0, trace: 'on-first-retry' },
+  workers: 1,
 };
 
 export default config;
