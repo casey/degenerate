01fd476a13cb75255ac44a66605d3217e78cc6f4
Liam <liam@scalzulli.com>
2022-06-30 19:01:13 -0400

    Explain wrap poorly

--- examples/kaleidoscope.js
+++ examples/kaleidoscope.js
@@ -10,7 +10,12 @@ circle();
 // Set the scale factor to 0.75
 scale(0.75);
 
-// Wrap pixels
+// Wrap sampled pixels
+//
+// Calls to `render` apply a transformation matrix
+// to the sampling coordinates, if those coordinates fall out of
+// bounds and `wrap` is toggled on, we wrap the transformed coordinates
+// between [-1, 1], else we use the default color.
 wrap();
 
 // Render to the canvas 8 times
--- examples/x.js
+++ examples/x.js
@@ -10,8 +10,10 @@ scale(0.5);
 // Render to the canvas 8 times, each time
 // toggling the `wrap` option.
 //
-// This will wrap sampled pixels in between [-1, 1]
-// instead of using the default color.
+// Calls to `render` apply a transformation matrix
+// to the sampling coordinates, if those coordinates fall out of
+// bounds and `wrap` is toggled on, we wrap the transformed coordinates
+// between [-1, 1], else we use the default color.
 for (_ of range(8)) {
   render();
   wrap();
