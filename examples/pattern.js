442c71e0c35b12a5d9f4fab2576d3ad36eeafde4
Casey Rodarmor <casey@rodarmor.com>
2022-07-01 01:26:12 -0700

    Make the alpha example use checkbox

--- examples/pattern.js
+++ examples/pattern.js
@@ -1,12 +1,15 @@
-reboot();
-
-alpha(0.75);
-
-circle();
-
-scale(0.5);
-
-for (_ of range(8)) {
-  render();
-  wrap();
+while (true) {
+  reboot();
+  if (checkbox('alpha')) {
+    alpha(0.75);
+  } else {
+    alpha(1);
+  }
+  circle();
+  scale(0.5);
+  for (_ of range(8)) {
+    render();
+    wrap();
+  }
+  await frame();
 }
