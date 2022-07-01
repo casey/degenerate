c0e0eed13117469c97bb7e6f314c0b4ccd2d397b
Casey Rodarmor <casey@rodarmor.com>
2022-07-01 12:18:59 -0700

    Umerge kaleidoscope examples

--- examples/kaleidoscope.js
+++ examples/kaleidoscope.js
@@ -1,21 +1,29 @@
-reboot();
+while(true) {
+  reboot();
 
-rotateColor('green', 0.05 * TAU);
+  rotateColor('green', 0.05 * TAU);
 
-circle();
+  circle();
 
-scale(0.75);
+  scale(0.75);
 
-wrap();
+  wrap();
 
-for (_ of range(8)) {
-  render();
-}
+  for (_ of range(8)) {
+    render();
+  }
 
-rotate((5 / 6) * TAU);
+  if checkbox('rotate') {
+    rotate((5 / 6) * TAU * elapsed() / 20000);
+  } else {
+    rotate((5 / 6) * TAU);
+  }
 
-rotateColor('blue', 0.05 * TAU);
+  rotateColor('blue', 0.05 * TAU);
 
-for (_ of range(8)) {
-  render();
+  for (_ of range(8)) {
+    render();
+  }
+
+  await frame();
 }
