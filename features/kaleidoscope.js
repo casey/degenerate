7936197283fdba8097caeb47594a454cde050dd9
liam <31192478+terror@users.noreply.github.com>
2022-07-05 19:08:22 -0400

    Make functions idempotent (#418)

--- features/kaleidoscope.js
+++ features/kaleidoscope.js
@@ -1,11 +1,11 @@
 rotateColor('green', 0.05 * TAU);
 circle();
 scale(0.75);
-wrap();
+wrap(true);
 for (let i = 0; i < 8; i++) {
   render();
 }
-rotate(0.8333 * TAU);
+rotate(state.rotation + 0.8333 * TAU);
 rotateColor('blue', 0.05 * TAU);
 for (let i = 0; i < 8; i++) {
   render();
