c6c25a1b08441c07e3026c3d2c1db5d042806daa
Casey Rodarmor <casey@rodarmor.com>
2022-07-13 14:56:47 -0700

    tweak

--- www/worker.js
+++ www/worker.js
@@ -81,7 +81,7 @@ function checkbox(name) {
       name,
       widget: 'checkbox',
     }
-  }))
+  }));
   return !!widgets['checkbox-' + name];
 }
 
@@ -350,7 +350,7 @@ function radio(name, options) {
         radio: {options},
       },
     }
-  }))
+  }));
   return widgets['radio-' + name] ?? options[0];
 }
 
