d35cf462da317711ed82e221fe22f92f2f57e965
Casey Rodarmor <casey@rodarmor.com>
2022-07-15 02:42:53 -0700

    tweak

--- features/diamonds.js
+++ features/diamonds.js
@@ -1,12 +1,12 @@
 rotateColor('green', 0.05 * TAU);
 circle();
 scale(2.0);
-transform(0.3333 * TAU, [2.0, 2.0], [0.0, 0.0])
+transform(0.3333 * TAU, [2.0, 2.0], [0.0, 0.0]);
 wrap(true);
 for (let i = 0; i < 8; i++) {
   render();
 }
-transform(0.3333 * TAU + 0.8333 * TAU, [2.0, 2.0], [0.0, 0.0])
+transform(0.3333 * TAU + 0.8333 * TAU, [2.0, 2.0], [0.0, 0.0]);
 rotateColor('blue', 0.05 * TAU);
 for (let i = 0; i < 8; i++) {
   render();
--- features/kaleidoscope.js
+++ features/kaleidoscope.js
@@ -1,11 +1,11 @@
 rotateColor('green', 0.05 * TAU);
 circle();
-scale(1/0.75);
+scale(1 / 0.75);
 wrap(true);
 for (let i = 0; i < 8; i++) {
   render();
 }
-transform(0.8333 * TAU, [1/0.75, 1/0.75], [0, 0]);
+transform(0.8333 * TAU, [1 / 0.75, 1 / 0.75], [0, 0]);
 rotateColor('blue', 0.05 * TAU);
 for (let i = 0; i < 8; i++) {
   render();
