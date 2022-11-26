27ff7527e093967a33c8be20eee18a66ace4bcc8
Casey Rodarmor <casey@rodarmor.com>
2022-11-25 22:00:09 -0800

    Sort

--- src/lib.rs
+++ src/lib.rs
@@ -16,20 +16,28 @@ pub enum AppMessage {
   },
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub enum WorkerMessage {
-  Clear,
-  DecibelRange { min: f32, max: f32 },
-  Done,
-  Error(String),
-  OscillatorFrequency(f32),
-  OscillatorGain(f32),
-  Record,
-  Render(Filter),
-  Resolution(u32),
-  Save,
-  Widget { name: String, widget: Widget },
+pub struct Filter {
+  pub alpha: f32,
+  pub color_transform: [f32; 16],
+  pub coordinate_transform: [f32; 9],
+  pub coordinates: bool,
+  pub default_color: [f32; 3],
+  pub field: u32,
+  pub field_mod_divisor: u32,
+  pub field_mod_remainder: u32,
+  pub field_rows_rows: u32,
+  pub field_rows_step: u32,
+  pub wrap: bool,
+}
+
+pub trait Program {
+  fn new() -> Self
+  where
+    Self: Sized;
+
+  fn on_message(&mut self, message: AppMessage);
 }
 
 #[derive(Serialize, Deserialize)]
@@ -65,42 +73,24 @@ impl Widget {
   }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Filter {
-  pub alpha: f32,
-  pub color_transform: [f32; 16],
-  pub coordinate_transform: [f32; 9],
-  pub coordinates: bool,
-  pub default_color: [f32; 3],
-  pub field: u32,
-  pub field_mod_divisor: u32,
-  pub field_mod_remainder: u32,
-  pub field_rows_rows: u32,
-  pub field_rows_step: u32,
-  pub wrap: bool,
-}
-
-pub trait Program {
-  fn new() -> Self
-  where
-    Self: Sized;
-
-  fn on_message(&mut self, message: AppMessage);
+pub enum WorkerMessage {
+  Clear,
+  DecibelRange { min: f32, max: f32 },
+  Done,
+  Error(String),
+  OscillatorFrequency(f32),
+  OscillatorGain(f32),
+  Record,
+  Render(Filter),
+  Resolution(u32),
+  Save,
+  Widget { name: String, widget: Widget },
 }
 
 pub fn execute<T: Program + 'static>() {
   execute_inner(Box::new(T::new()));
-}
-
-pub fn post_message(worker_message: WorkerMessage) {
-  let global = js_sys::global().unchecked_into::<DedicatedWorkerGlobalScope>();
-
-  global
-    .post_message(&JsValue::from_str(
-      &serde_json::to_string(&worker_message).unwrap(),
-    ))
-    .unwrap();
 }
 
 fn execute_inner(mut program: Box<dyn Program>) {
@@ -114,4 +104,14 @@ fn execute_inner(mut program: Box<dyn Program>) {
     .unwrap();
 
   closure.forget();
+}
+
+pub fn post_message(worker_message: WorkerMessage) {
+  let global = js_sys::global().unchecked_into::<DedicatedWorkerGlobalScope>();
+
+  global
+    .post_message(&JsValue::from_str(
+      &serde_json::to_string(&worker_message).unwrap(),
+    ))
+    .unwrap();
 }
